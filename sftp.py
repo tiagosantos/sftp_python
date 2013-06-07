16914d73efb1b578e452ba92136305b1c9838057
Tiago Santos <ontiago@gmail.com>
2013-06-07 16:36:01 -0300

    Update sftp.py

--- sftp.py
+++ sftp.py
@@ -14,33 +14,50 @@ arquivo = sys.argv[3]
 paramiko.util.log_to_file('./sftp.log')
 
 #acrescente o endereço do servidor abaixo
-host = 'www.dominio.com'
+host = 'ftp.s4.exacttarget.com'
 port = 22
 
+
 #este bloco realiza a conexão
-transport = paramiko.Transport((host, port))
-transport.connect(username = username, password = password)
-sftp = paramiko.SFTPClient.from_transport(transport)
+print 'Estabelecendo conexao com o servidor: ', host, ' porta: ', port, '...'
+try:
+	transport = paramiko.Transport((host, port))
+	transport.connect(username = username, password = password)
+	sftp = paramiko.SFTPClient.from_transport(transport)
+except:
+	print '*** Erro ao se conectar com o servidor: %s: %s' % (e.__class__, e)
+    	sys.exit()
 
 #acrescente o diretório onde estão os arquivos que serão transferidos
-caminho_local = './diretorio_local/'
+caminho_alphabase = './arquivos/'
 #acrescente o diretório onde serão transferidos os arquivos
-caminho_remoto = './diretorio_remoto/'
-
-
+caminho_exact = './Import/'
 #cria extensão de arquivo temporatio (.TMP)
-filepath = caminho_remoto + arquivo + '.TMP'
-
-localpath = caminho_local + arquivo
+filepath = caminho_exact + arquivo + '.TMP'
+localpath = caminho_alphabase + arquivo 
+new_filepath = caminho_exact + arquivo
 
 #transfere o arquivo
-sftp.put(localpath, filepath)
+try:
+	sftp.put(localpath, filepath)
+	print 'Arquivo', arquivo,'tranferido!'
+except:
+	print '*** Erro na tranferencia do arquivo:', arquivo
+
 
 #se o arquivo foi transferido corretamente, retira a extensão de temporario
-new_filepath = caminho_remoto + arquivo 
-sftp.rename(filepath, new_filepath)
+try:
+    sftp.rename(filepath, new_filepath)
+    print '    Arquivo ', arquivo,' transferido com sucesso! '
+except:
+	sftp.remove(new_filepath)
+	print '*** Erro ao renomear arquivo! O arquivo', arquivo, 'ja existe e sera substituido! ' 
+finally:
+	sftp.rename(filepath, new_filepath)
 
 
 #finaliza conexão
-sftp.close()
-transport.close()
+try:
+	sftp.close()
+	transport.close()
+	print 'Tranferencia finalizada com sucesso!'
