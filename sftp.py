8a1fb9f582f83e2f8301f09701a7e1675f4a80bc
Tiago Santos <ontiago@gmail.com>
2013-06-27 19:41:35 -0300

    update sftp.py
    Adicionado parâmetros para linha de comando

--- sftp.py
+++ sftp.py
@@ -8,31 +8,35 @@ from ConfigParser import RawConfigParser
 #este bloco recebe os parametros
 username = sys.argv[1]
 password = sys.argv[2]
-arquivo = sys.argv[3]
-
+host = sys.argv[3]
+#acrescente o diretório onde estao os arquivos que serao transferidos
+caminho_local = sys.argv[4]
+#acrescente o diretório onde serao transferidos os arquivos
+caminho_host = sys.argv[5]
+arquivo = sys.argv[6]
 #cria arquivo de log da conexao sftp
 paramiko.util.log_to_file('./sftp.log')
 
 #acrescente o endereço do servidor abaixo
-host = 'dominio.com'
 port = 22
 
 
-#este bloco realiza a conexão
-print 'Estabelecendo conexao com o servidor: ', host, ' porta: ', port, '...'
-try:
-	transport = paramiko.Transport((host, port))
-	transport.connect(username = username, password = password)
-	sftp = paramiko.SFTPClient.from_transport(transport)
-except:
-	print '*** Erro ao se conectar com o servidor: %s: %s' % (e.__class__, e)
-    	sys.exit()
+#este bloco realiza a conexao
 
-#acrescente o diretório onde estão os arquivos que serão transferidos
-caminho_local = './arquivos/'
-#acrescente o diretório onde serão transferidos os arquivos
-caminho_host = './Diretorio/'
-#cria extensão de arquivo temporatio (.TMP)
+try:
+        print '*** Estabelecendo conexao com o servidor: ', host, ' porta: ', port, '...'
+        transport = paramiko.Transport((host, port))
+        transport.connect(username = username, password = password)
+        sftp = paramiko.SFTPClient.from_transport(transport)
+        print '*** Conexao realizada'
+except:
+        print '*** Erro ao se conectar com o servidor: %s: %s' % (e.__class__, e)
+        f_log_sftp = open("./sftp.log","r")
+        log_sftp = f_log_sftp.read()
+        print log_sftp
+        sys.exit()
+
+#cria extensao de arquivo temporatio (.TMP)
 filepath = caminho_host + arquivo + '.TMP'
 localpath = caminho_local + arquivo 
 new_filepath = caminho_host + arquivo
@@ -41,25 +45,41 @@ new_filepath = caminho_host + arquivo
 #se precisar fazer download, use get ao invés de put
 #exemplo: sftp.get(filepath, localpath)
 try:
-	sftp.put(localpath, filepath)
-	print 'Arquivo', arquivo,'tranferido!'
+        sftp.put(localpath, filepath)
 except:
-	print '*** Erro na tranferencia do arquivo:', arquivo
+        print '*** Erro na tranferencia do arquivo:', arquivo
+        f_log_sftp = open("./sftp.log","r")
+        log_sftp = f_log_sftp.read()
+        print log_sftp
+        sys.exit()
 
 
-#se o arquivo foi transferido corretamente, retira a extensão de temporario
+#se o arquivo foi transferido corretamente, retira a extensao de temporario
 try:
-    sftp.rename(filepath, new_filepath)
-    print '    Arquivo ', arquivo,' transferido com sucesso! '
+        sftp.rename(filepath, new_filepath)
+        print '*** Arquivo ', arquivo,' transferido com sucesso! '
+        print '*** Tranferencia finalizada com sucesso!'
 except:
-	sftp.remove(new_filepath)
-	print '*** Erro ao renomear arquivo! O arquivo', arquivo, 'ja existe e sera substituido! ' 
-finally:
-	sftp.rename(filepath, new_filepath)
+        sftp.remove(new_filepath)
+        print '*** Arquivo ', arquivo, 'ja existe e sera substituido '
+        try:
+                sftp.rename(filepath, new_filepath)
+                print '*** Tranferencia finalizada com sucesso!'
+        except:
+                print '*** Erro ao renomear arquivo',arquivo
+                sys.exit()
 
 
-#finaliza conexão
+#finaliza conexao
 try:
-	sftp.close()
-	transport.close()
-	print 'Tranferencia finalizada com sucesso!'
+        sftp.close()
+        transport.close()
+        print '*** Conexao finalizada com sucesso!'
+except:
+        print '*** Erro ao finalizar conexao!'
+        f_log_sftp = open("./sftp.log","r")
+        log_sftp = f_log_sftp.read()
+        print log_sftp
+        sys.exit()
+
+sys.exit()
